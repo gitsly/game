5520a4aa18292fc7005320e2322e779d57279ed7
Martin <martin.collberg@gmail.com>
2012-05-12 21:42:30 +0200

    initial commit, tutorial window

---
+++ BasicWindow/Program.cs
@@ -0,0 +1,14 @@
+﻿using SlimDX.Windows;
+
+namespace BasicWindow
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var form = new RenderForm("Tutorial 1: Basic Window");
+
+            MessagePump.Run(form, () => { });
+        }
+    }
+}

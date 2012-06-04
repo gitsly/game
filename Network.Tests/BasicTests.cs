66642c24f3b51c086bf4d1f3deeb68e0b76d6766
Martin <martin.collberg@gmail.com>
2012-06-04 22:00:41 +0200

    started network module

---
+++ Network.Tests/BasicTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Network;
+using System.Net;
+
+namespace Network.Tests
+{
+    [TestFixture]
+    public class BasicTests
+    {
+
+        [Test]
+        public void TestResolveUtilityMethod()
+        {
+            var addressesToLocalHost = Utils.ResolveHost("localhost");
+            foreach (var addr in addressesToLocalHost)
+            {
+                Console.WriteLine(addr);
+            }
+        }
+
+        [Test]
+        public void TestConnectAClientToAServer()
+        {
+            var testClient = new Client();
+            var testServer = new Server();
+
+            testServer.OpenServerConnection(991);
+
+            testClient.BeginConnect("127.0.0.1", 991);
+
+        }
+    }
+}
---
+++ Network/Server.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+
+// http://ondotnet.com/pub/a/dotnet/2002/10/21/sockets.htm
+// http://www.csharp-examples.net/socket-send-receive/
+
+namespace Network
+{
+    public class Utils
+    {
+        public static IPAddress[] ResolveHost(string hostname)
+        {
+            try
+            {
+                IPHostEntry hostEntry = Dns.GetHostEntry(hostname);
+                IPAddress[] ipAddresses = hostEntry.AddressList;
+                return ipAddresses;
+            }
+            catch (Exception e)
+            {
+                return null;       
+            }
+        }
+    }
+
+
+    // A server keeps multiple ClientConnection to handle outgoing traffic
+    internal class ClientConnection
+    {
+        internal Socket ClientSocket { get; private set; }
+    }
+
+    public class Server
+    {
+        public Socket serverSocket { get; private set; }
+
+        public Server()
+        {
+            Console.WriteLine("created base networking object");
+        }
+
+
+        public void OpenServerConnection(int PortNumber)
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+
+        }
+    }
+
+    public class Client
+    {
+        Socket ClientSocket { get; set; }
+
+        // Asynchronous connect to host.
+        public void BeginConnect(string hostName, int port)
+        {
+            ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            var ipEndPoint = new IPEndPoint(Utils.ResolveHost(hostName)[0], port);
+            ClientSocket.BeginConnect(ipEndPoint, EndConnection, null);
+        }
+
+        public void EndConnection(IAsyncResult result)
+        {
+        }
+    }
+
+}

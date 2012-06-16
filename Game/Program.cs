d6bc38420f6f071cd07ba7a9676f0a3acc034f6e
Martin <martin.collberg@gmail.com>
2012-06-16 21:24:52 +0200

    made triangle into a renderobject, trying to separate duties on a crude level, lets see what needs to be updated each frame

--- Game/Program.cs
+++ Game/Program.cs
@@ -16,9 +16,6 @@ namespace Game
             Device device;
             SwapChain swapChain;
 
-            ShaderSignature vsInputSignature;
-            VertexShader vertexShader;
-            PixelShader pixelShader;
 
             var form = new RenderForm("Test Game");
             var description = new SwapChainDescription()
@@ -40,71 +37,12 @@ namespace Game
             using (var resource = Resource.FromSwapChain<Texture2D>(swapChain, 0))
                 renderTarget = new RenderTargetView(device, resource);
 
+            var viewport = new Viewport(0.0f, 0.0f, form.ClientSize.Width, form.ClientSize.Height);
+
             // setting a viewport is required if you want to actually see anything
             var context = device.ImmediateContext;
-            var viewport = new Viewport(0.0f, 0.0f, form.ClientSize.Width, form.ClientSize.Height);
             context.OutputMerger.SetTargets(renderTarget);
             context.Rasterizer.SetViewports(viewport);
-
-            // load and compile the vertex shader
-            using (var bytecode = ShaderBytecode.CompileFromFile("simple.fx", "VShader", "vs_4_0", ShaderFlags.None, EffectFlags.None))
-            {
-                vsInputSignature = ShaderSignature.GetInputSignature(bytecode);
-                vertexShader = new VertexShader(device, bytecode);
-            }
-
-            // load and compile the pixel shader
-            using (var bytecode = ShaderBytecode.CompileFromFile("simple.fx", "PShader", "ps_4_0", ShaderFlags.None, EffectFlags.None))
-                pixelShader = new PixelShader(device, bytecode);
-
-            const int vertexSize = 24;
-
-            // create test vertex data, making sure to rewind the stream afterward
-            var vertices = new DataStream(vertexSize * 3, true, true);
-            vertices.Write(new Vector3(0.0f, 0.5f, 0.5f));
-            vertices.Write(new Vector3(1, 0, 0)); // color
-            vertices.Write(new Vector3(0.5f, -0.5f, 0.5f));
-            vertices.Write(new Vector3(0, 1, 0)); // color
-            vertices.Write(new Vector3(-0.5f, -0.5f, 0.5f));
-            vertices.Write(new Vector3(0, 0, 1)); // color
-            vertices.Position = 0;
-
-            // create the vertex layout and buffer
-            var elements = new[] { 
-                new InputElement("POSITION", 0, Format.R32G32B32_Float, 0),
-                new InputElement("COLOR", 0, Format.R32G32B32_Float, 0)
-            };
-                
-            // Using the effect classes (not used yet), each effect pass will have a signature (for the vertex shader used) in its effect description you can use to create the input layout.
-            // You can re-use these layouts with other shaders that have identical signatures.
-
-            var layout = new InputLayout(device, vsInputSignature, elements);
-            var vertexBuffer = new Buffer(device, vertices, vertexSize * 3, ResourceUsage.Default, BindFlags.VertexBuffer, CpuAccessFlags.None, ResourceOptionFlags.None, 0);
-
-            // configure the Input Assembler portion of the pipeline with the vertex data
-            context.InputAssembler.InputLayout = layout;
-            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
-            context.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(vertexBuffer, vertexSize, 0));
-
-            // set the shaders
-            context.VertexShader.Set(vertexShader);
-            context.PixelShader.Set(pixelShader);
-
-            Matrix matrix = Matrix.Identity;
-
-            matrix.M42 = 1.0f;
-
-            // Setup Constant Buffers
-            const int matrixSize = (sizeof(float) * 4 * 4); // 4 rows, each with 4 values - x,y,z,w
-            DataStream constantStream;
-            constantStream = new DataStream(matrixSize, true, true);
-            constantStream.Write(matrix);
-            constantStream.Position = 0; // rewind stream.
-
-
-            var constantBuffer = new Buffer(device, constantStream, matrixSize * 1, ResourceUsage.Dynamic, BindFlags.ConstantBuffer, CpuAccessFlags.Write, ResourceOptionFlags.None, 0);
-                                         
-            context.VertexShader.SetConstantBuffer(constantBuffer, 0);
 
             // prevent DXGI handling of alt+enter, which doesn't work properly with Winforms
             using (var factory = swapChain.GetParent<Factory>())
@@ -129,27 +67,23 @@ namespace Game
                 context.OutputMerger.SetTargets(renderTarget);
             };
 
+
+            // Create a renderobject
+            var renderObject = new RenderObject(device);
+
             MessagePump.Run(form, () =>
             {
                 // clear the render target to a soothing blue
                 context.ClearRenderTargetView(renderTarget, new Color4(0.5f, 0.5f, 1.0f));
 
-                // USe this to set camera matrix. 
-                // context.VertexShader.SetConstantBuffer()
+                renderObject.Render(context);
 
-                // draw the triangle
-                context.Draw(3, 0);
                 swapChain.Present(0, PresentFlags.None);
             });
 
             // clean up all resources
             // anything we missed will show up in the debug output
-            vertices.Close();
-            vertexBuffer.Dispose();
-            layout.Dispose();
-            vsInputSignature.Dispose();
-            vertexShader.Dispose();
-            pixelShader.Dispose();
+            renderObject.Dispose();
             renderTarget.Dispose();
             swapChain.Dispose();
             device.Dispose();
---
+++ Game/RenderObject.cs
@@ -0,0 +1,117 @@
+﻿using SlimDX;
+using SlimDX.D3DCompiler;
+using SlimDX.Direct3D11;
+using SlimDX.DXGI;
+using SlimDX.Windows;
+using Device = SlimDX.Direct3D11.Device;
+using Resource = SlimDX.Direct3D11.Resource;
+using System;
+
+namespace Game
+{
+    public class RenderObject : IDisposable
+    {
+        private ShaderSignature vsInputSignature;
+        private VertexShader vertexShader;
+        private PixelShader pixelShader;
+
+        private DataStream vertices;
+        private SlimDX.Direct3D11.Buffer vertexBuffer;
+        private SlimDX.Direct3D11.Buffer constantBuffer;
+
+        private InputLayout layout;
+
+        private int vertexSize;
+        private int vertexCount;
+
+        public void Dispose()
+        {
+            vertices.Close();
+
+            constantBuffer.Dispose();
+            vertexBuffer.Dispose();
+
+            layout.Dispose();
+            vsInputSignature.Dispose();
+            vertexShader.Dispose();
+            pixelShader.Dispose();
+
+        }
+
+
+        public RenderObject(Device device)
+        {
+            // load and compile the vertex shader
+            using (var bytecode = ShaderBytecode.CompileFromFile("simple.fx", "VShader", "vs_4_0", ShaderFlags.None, EffectFlags.None))
+            {
+                vsInputSignature = ShaderSignature.GetInputSignature(bytecode);
+                vertexShader = new VertexShader(device, bytecode);
+            }
+
+            // load and compile the pixel shader
+            using (var bytecode = ShaderBytecode.CompileFromFile("simple.fx", "PShader", "ps_4_0", ShaderFlags.None, EffectFlags.None))
+                pixelShader = new PixelShader(device, bytecode);
+
+            vertexSize = 24;
+            vertexCount = 3;
+
+            // create test vertex data, making sure to rewind the stream afterward
+            vertices = new DataStream(vertexSize * vertexCount, true, true);
+            vertices.Write(new Vector3(0.0f, 0.5f, 0.5f));
+            vertices.Write(new Vector3(1, 0, 0)); // color
+            vertices.Write(new Vector3(0.5f, -0.5f, 0.5f));
+            vertices.Write(new Vector3(0, 1, 0)); // color
+            vertices.Write(new Vector3(-0.5f, -0.5f, 0.5f));
+            vertices.Write(new Vector3(0, 0, 1)); // color
+            vertices.Position = 0;
+
+            // create the vertex layout and buffer
+            var elements = new[] { 
+                new InputElement("POSITION", 0, Format.R32G32B32_Float, 0),
+                new InputElement("COLOR", 0, Format.R32G32B32_Float, 0)
+            };
+
+            // Using the effect classes (not used yet), each effect pass will have a signature (for the vertex shader used) in its effect description you can use to create the input layout.
+            // You can re-use these layouts with other shaders that have identical signatures.
+
+            layout = new InputLayout(device, vsInputSignature, elements);
+            vertexBuffer = new SlimDX.Direct3D11.Buffer(device, vertices, vertexSize * vertexCount, ResourceUsage.Default, BindFlags.VertexBuffer, CpuAccessFlags.None, ResourceOptionFlags.None, 0);
+
+
+            // Setup Constant Buffers
+            Matrix matrix = Matrix.Identity;
+            matrix.M42 = 1.0f;
+
+            const int matrixSize = (sizeof(float) * 4 * 4); // 4 rows, each with 4 values - x,y,z,w
+            DataStream constantStream;
+            constantStream = new DataStream(matrixSize, true, true);
+            constantStream.Write(matrix);
+            constantStream.Position = 0; // rewind stream.
+
+            constantBuffer = new SlimDX.Direct3D11.Buffer(device, constantStream, matrixSize * 1, ResourceUsage.Dynamic, BindFlags.ConstantBuffer, CpuAccessFlags.Write, ResourceOptionFlags.None, 0);
+
+            
+        }
+
+        private bool temp = false;
+
+        public void Render(DeviceContext context)
+        {
+            // configure the Input Assembler portion of the pipeline with the vertex data
+            context.InputAssembler.InputLayout = layout;
+            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+            context.InputAssembler.SetVertexBuffers(0, new VertexBufferBinding(vertexBuffer, vertexSize, 0));
+
+            // set the shaders
+            context.VertexShader.Set(vertexShader);
+            context.PixelShader.Set(pixelShader);
+
+            context.VertexShader.SetConstantBuffer(constantBuffer, 0);
+
+            // draw the triangle
+            context.Draw(vertexCount, 0);
+        }
+
+
+    }
+}
